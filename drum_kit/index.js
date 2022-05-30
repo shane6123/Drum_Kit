2248655a2e5386b3ff147839fdb8bae35fbdeb4f
shanealam6123 <shanealam6123@gmail.com>
2022-05-30 13:11:00 +0530

    first

---
+++ drum_kit/index.js
@@ -0,0 +1,59 @@
+// document.querySelector("button").addEventListener("click",handclick)
+// function handclick(){
+//     alert("ckick");
+// }
+// annomynus fuction
+var noOfDrum = document.querySelectorAll(".drum").length;
+for (var i = 0; i < noOfDrum; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function handclick() {
+    //   console.log(this.innerHTML)
+     
+      var buttonInnerHtml = this.innerHTML;
+     makesound(buttonInnerHtml)
+     buttonAnimation(buttonInnerHtml)
+    });
+}
+document.addEventListener("keypress",function(event){
+    makesound(event.key)
+    buttonAnimation(event.key)
+ 
+});
+function makesound(key){
+    switch (key) {
+        case "w":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+        case"a":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+        case "s":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+        case "d":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+        case "j":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+        case "k":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+        case "l":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+    }
+}
+function buttonAnimation(currentKey){
+    var activeButton=document.querySelector("."+currentKey)
+    activeButton.classList.add("pressed")
+    setTimeout( function(){
+        activeButton.classList.remove("pressed")
+    },100)
+}
